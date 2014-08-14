ee581bbff6d8a14d3ac9aa3874acc5a8c8b6eb42
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-08-14 10:46:16 +0200

    io file

--- Recommender Systems/src/fileIO/ReadTXTPaolo.java
+++ Recommender Systems/src/fileIO/ReadTXTPaolo.java
@@ -13,10 +13,13 @@ public class ReadTXTPaolo {
 			FileReader fileReader = new FileReader(file);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			StringBuffer stringBuffer = new StringBuffer();
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				stringBuffer.append(line);
-				stringBuffer.append("\n");
+			String inputLine;
+			String[] splitedString;
+			while ((inputLine = bufferedReader.readLine()) != null) {
+				//System.out.println(inputLine);
+				splitedString = inputLine.split("\\s+");
+				System.out.println(splitedString[1]);
+//				
 			}
 			fileReader.close();
 			System.out.println(stringBuffer.toString());
