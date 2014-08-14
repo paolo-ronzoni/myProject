75152209eb84dda01326aebea86ad390c67dc0bb
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-08-14 10:30:25 +0200

    add u.txt data of compelte Movie ratings (100000 values)

--- Recommender Systems/src/fileIO/ReadTXTPaolo.java
+++ Recommender Systems/src/fileIO/ReadTXTPaolo.java
@@ -1,7 +1,27 @@
 package fileIO;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReadTXTPaolo {
-
-	
-
+	static String path1 = "DataStore\\prova.txt";
+	public static void main(String[] args) {
+		try {
+			File file = new File(path1);
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				stringBuffer.append(line);
+				stringBuffer.append("\n");
+			}
+			fileReader.close();
+			System.out.println(stringBuffer.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
