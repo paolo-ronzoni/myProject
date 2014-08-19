b7b87683e4f6dde46f1f57fa6793f472e5ac610d
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-08-19 10:56:40 +0200

    cut useless code form ReadTXTPaolo Class

--- Recommender Systems/src/fileIO/ReadTXTPaolo.java
+++ Recommender Systems/src/fileIO/ReadTXTPaolo.java
@@ -8,29 +8,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class ReadTXTPaolo {
-	static String path1 = "DataStore\\prova.txt";
-	
-	public static void main(String[] args) {
-	
-		
-		try {
-			File file = new File(path1);
-			FileReader fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			String inputLine;
-			String[] splitedString;
-			while ((inputLine = bufferedReader.readLine()) != null) {
-				
-				splitedString = inputLine.split("\\s+");
-				System.out.println(splitedString[0]);			
-			}
-			fileReader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	
-	}
+
 	
 	/**
 	 * @param filePath location of the .txt file
