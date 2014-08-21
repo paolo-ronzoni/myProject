fb192fd1b3863ea32f1fdd4bc11b1603f9281fdc
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-08-21 13:48:47 +0200

    adds package collaborative Recommendation and class MatrixBuilder

--- Recommender Systems/src/fileIO/TestClass.java
+++ Recommender Systems/src/fileIO/TestClass.java
@@ -1,22 +1,22 @@
 package fileIO;
 
-import fileIO.IOMethods;
+
 	
 	
 public class TestClass {
 	static double[][] doubleMatrix = {{23.2, 11.5, 18.23}, 
 									  {7.89, 56.1, 1.00}};
 	static String path1 = "DataStore\\prova.txt";
-	static String path2 = "DataStore\\u1.txt";
+	static String path2 = "DataStore\\doubleNumbers.txt";
 	static String splitExpression = "\\s+";
 	public static void main(String[] args) {
           
-	String[][] myMatrix = IOMethods.txtMatrix(path1, splitExpression);
+	String[][] myMatrix = IOMethods.txtMatrix(path2, splitExpression);
 	IOMethods.printMatrix(myMatrix, "%4s\t");
-	int[][] integerMatrix = IOMethods.matrixConversion(myMatrix);
-	double[] matrix1 = IOMethods.columnExtractor(doubleMatrix, 2);
+	double[][] secondMatrix = IOMethods.matrixConversionDouble(myMatrix);
+	double[] matrix1 = IOMethods.columnExtractor(secondMatrix, 0);
 	IOMethods.printMatrix(matrix1, "%4s\t");
-//	IOMethods.printMatrix(doubleMatrix, "%4s\t");
+	IOMethods.printMatrix(secondMatrix, "%4s\t");
 	
 	}// end main
 			
