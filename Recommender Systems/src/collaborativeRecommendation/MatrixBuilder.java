a67304be528d83b6d0920327e828d18e02e73cfb
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-08-21 14:01:51 +0200

    delete class TestRecommendation

--- Recommender Systems/src/collaborativeRecommendation/MatrixBuilder.java
+++ Recommender Systems/src/collaborativeRecommendation/MatrixBuilder.java
@@ -2,4 +2,20 @@ package collaborativeRecommendation;
 
 public class MatrixBuilder {
 
+	
+	/** Overloads columnExtractor
+	 * by Paolo Ronzoni
+	 * @param inputMatrix an input matrix of type double[][]
+	 * @param columnNumber the number of the column
+	 * @return outputArray an array of the column extracts
+	 */
+	public static double[] columnExtractor(double[][] inputMatrix, int columnNumber) {
+		int nRows = inputMatrix.length;
+		double[] outputArray = new double[nRows ];
+		for (int row = 0; row < nRows; row++)
+		{
+		    outputArray[row] = inputMatrix[row][columnNumber];		    
+		} 			 
+		return outputArray;
+	 } // end method columnExtractor
 }
