2913bda514caccb7d47062a967fa0c29650025be
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-08-22 08:54:42 +0200

    adds README to the DataSet

--- Recommender Systems/src/outputView/TestResult.java
+++ Recommender Systems/src/outputView/TestResult.java
@@ -8,7 +8,7 @@ public class TestResult {
 
 	static double[][] doubleMatrix = {{23.2, 11.5, 18.23}, 
 		  {7.89, 56.1, 1.00}};
-static String path1 = "DataStore\\u1.txt";
+static String path1 = "DataStore\\u.txt";
 static String path2 = "DataStore\\doubleNumbers.txt";
 static String splitExpression = "\\s+";
 	
@@ -23,11 +23,15 @@ public static void main(String[] args) {
 	// IOMethods.printMatrix(secondMatrix, "%4s\t");
 
 
-int elementFounded = MatrixBuilder.numberOfElement(secondMatrix, 0, 186);
-System.out.println("element founded: " + elementFounded);
-System.out.println("matrix length: " + secondMatrix.length);
-int [][] finalMatrix = MatrixBuilder.userIDchoices(secondMatrix, 0, 3);
-IOMethods.printMatrix(finalMatrix, "%4s\t");
+//	int elementFounded = MatrixBuilder.numberOfElement(secondMatrix, 0, 186);
+//	System.out.println("element founded: " + elementFounded);
+//	System.out.println("matrix length: " + secondMatrix.length);
+     for (int i = 1; i < 30; i++) {
+    	int [][] finalMatrix = MatrixBuilder.userIDchoices(secondMatrix, 0, i);
+     }
+	
+// IOMethods.printMatrix(finalMatrix, "%4s\t");
+     System.out.println("fatto!");
 	} // end main
 
 } // end class
