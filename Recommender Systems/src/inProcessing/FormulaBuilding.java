aab7e49c904b92ea8f3185e7629a25db1127ab79
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-08-22 12:01:31 +0200

    adds intersection of two array code

--- Recommender Systems/src/inProcessing/FormulaBuilding.java
+++ Recommender Systems/src/inProcessing/FormulaBuilding.java
@@ -1,10 +1,26 @@
 package inProcessing;
+import java.util.*;
 
 public class FormulaBuilding {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String args[]) {
+	  
+		int a[] = {3, 10, 4, 2, 8};
+        int[] b = {10, 4, 12, 3, 23, 1, 8};
+        int[] c = new int[(int)Math.min(a.length, b.length)];
+        int i=0;
+         for(int f=0;f<a.length;f++){
+              for(int k=0;k<b.length;k++){
+                    if(a[f]==b[k]){
+                    c[i] = a[f];
+                    i++;
+            }
+          }
+        }
+        for (int x=0; x<i; x++){
+           System.out.println(c[x]);
+        }
+       
+	   } // end main
 
-	}
-
-}
+} // end Class FormulaBuilding
