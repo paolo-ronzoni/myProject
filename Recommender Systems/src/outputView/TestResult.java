3c2b794f283d165147b7880853836a1b05e7f951
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-08-25 14:11:12 +0200

    minor changes

--- Recommender Systems/src/outputView/TestResult.java
+++ Recommender Systems/src/outputView/TestResult.java
@@ -8,7 +8,7 @@ public class TestResult {
 
 	static double[][] doubleMatrix = {{23.2, 11.5, 18.23}, 
 		  {7.89, 56.1, 1.00}};
-static String path1 = "DataStore\\u1.txt";
+static String path1 = "DataStore\\u.txt";
 static String path2 = "DataStore\\u1.txt";
 static String splitExpression = "\\s+";
 	
@@ -16,7 +16,7 @@ static String splitExpression = "\\s+";
 		
 public static void main(String[] args) {
 
-	String[][] myMatrix = IOMethods.txtMatrix(path2, splitExpression);
+	String[][] myMatrix = IOMethods.txtMatrix(path1, splitExpression);
 	// IOMethods.printMatrix(myMatrix, "%4s\t");
 	int[][] secondMatrix = IOMethods.matrixConversion(myMatrix);
 	// int[] matrix1 = IOMethods.columnExtractor(secondMatrix, 0);
@@ -28,7 +28,7 @@ public static void main(String[] args) {
 //	System.out.println("matrix length: " + secondMatrix.length);
 	int [][] solutionMatrix = MatrixBuilder.matchIDchoices(secondMatrix, 0, 8, 1);
  IOMethods.printMatrix(solutionMatrix, "%4s\t");
- double nearestValue = NearestNeighborValue.nearestNeigborValue(secondMatrix, 0, 1, 1);
+ double nearestValue = NearestNeighborValue.nearestNeigborValue(secondMatrix, 0, 1, 45);
  int check = solutionMatrix[0][0];
  System.out.println("The nearestValue is: " + nearestValue);
      System.out.println("fatto!");
