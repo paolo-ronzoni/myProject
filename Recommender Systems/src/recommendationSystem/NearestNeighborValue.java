8e14a0c8936ad068f6b673727bdaf8948a64790b
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-08-25 11:43:32 +0200

    completed method "nearestNeigborValue"

--- Recommender Systems/src/recommendationSystem/NearestNeighborValue.java
+++ Recommender Systems/src/recommendationSystem/NearestNeighborValue.java
@@ -11,47 +11,34 @@ public class NearestNeighborValue {
 	 * @return a three column matrix with the common choices of the two users
 	 * @author Paolo Ronzoni
 	 */
-	public static int[][] nearestNeigborValue(int[][] inputMatrix, int columnNumber, int firstElementSearched, int secondElementSearched) {		
+	public static double nearestNeigborValue(int[][] inputMatrix, int columnNumber, int firstElementSearched, int secondElementSearched) {		
 		
 		// create a matrix of three column: items, user1scores, user2scores
 		int[][] intermediateMatrix = MatrixBuilder.matchIDchoices(inputMatrix, columnNumber,firstElementSearched, secondElementSearched);
 		
 		int user1columnScores = 1;
 		int user2columnScores = 2;
+		int numRows = inputMatrix.length;
+		double numerator = 0;
+		double denominator1 = 0;
+		double denominator2 = 0;
+		double finalValue = 0;
 		
 		// calculates the averages of the columns' users scores 
-		double firstUserAverage = StatAndMathTools.matrixColumnAverage(inputMatrix, user1columnScores);
-		double secondUserAverage = StatAndMathTools.matrixColumnAverage(inputMatrix, user2columnScores);
+		double User1Average = StatAndMathTools.matrixColumnAverage(inputMatrix, user1columnScores);
+		double User2Average = StatAndMathTools.matrixColumnAverage(inputMatrix, user2columnScores);
 		
 		
-		int[][] outputMatrix = new int[(int)Math.min(firstUserRows, secondUserRows)][3];
-		int userRow = 0;
-		int outPutRow = 0;
-			
-		for (int i = 0; i < firstUserRows  ; i++)
+		for (int row = 0; row < numRows  ; row++)
 		{
-			for (int j = 0; j < secondUserRows; j++) 
-			{
-		    if (firstUserMatrix[i][itemPosition] == secondUserMatrix[j][itemPosition] ) 
-			    {
-		    	    
-			    	outputMatrix[userRow][0] = secondUserMatrix[j][1];
-			    	outputMatrix[userRow][1] = firstUserMatrix[i][2];
-			    	outputMatrix[userRow][2] = secondUserMatrix[j][2];
-			    	if (outputMatrix[userRow][0] != 0) outPutRow++;
-			    	userRow++;
+		numerator += (intermediateMatrix[row][user1columnScores] - User1Average) * (intermediateMatrix[row][user2columnScores] - User2Average);
+		
+		denominator1 = Math.pow((intermediateMatrix[row][user1columnScores] - User1Average),2);
+		denominator2 = Math.pow((intermediateMatrix[row][user2columnScores] - User2Average),2);
 			    	
-			    }
-			}// end inner for
 		} // end outer for 	
 		
-		int[][] finalMatrix = new int[outPutRow][3];
-		for (int k = 0; k < outPutRow; k++) {
-			for (int column  = 0; column < 3; column++)
-			finalMatrix[k][column] = outputMatrix[k][column];
-		}
 		
-		
-		return finalMatrix;
+		return numerator / (Math.sqrt(denominator1 * denominator2));
 	 } // end method matchIdChoices
 }
---
+++ Recommender Systems/src/recommendationSystem/StatAndMathTools.java
@@ -0,0 +1,26 @@
+package recommendationSystem;
+
+public class StatAndMathTools {
+	
+	/** 
+	 * @param inputMatrix an input matrix of type int
+	 * @param columnNumber column where to do the computation
+	 * @return the average of the chosen column 
+	 * @author Paolo Ronzoni
+	 */
+	public static double matrixColumnAverage(int[][] inputMatrix, int columnNumber) {		
+		
+		int numberOfRows = inputMatrix.length;
+		double sumOfValues = 0;
+			
+		for (int row = 0; row < numberOfRows  ; row++)
+		{
+		    sumOfValues += inputMatrix[row][columnNumber];	    
+			 
+		} // end outer for 		
+		
+		return sumOfValues/numberOfRows;
+	 } // end method matrixColumnAverage
+
+}
+
