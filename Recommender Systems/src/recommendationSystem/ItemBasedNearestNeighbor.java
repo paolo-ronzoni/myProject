e7e3d328c1765c513024444add10c31ebcc745f1
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-09-08 15:21:44 +0200

    improvement to "predictionItemBasedValue"

--- Recommender Systems/src/recommendationSystem/ItemBasedNearestNeighbor.java
+++ Recommender Systems/src/recommendationSystem/ItemBasedNearestNeighbor.java
@@ -90,94 +90,34 @@ public class ItemBasedNearestNeighbor {
 	/** 
 	 * @param inputMatrix an input matrix of type int
 	 * @param usercolumnNumber the position of the column where to search the user
-	 * @param firstElementSearched the  user searched
+	 * @param userSearched the user that must be searched
 	 * @param itemcolumnNumber the position of the column where to search the item
 	 * @param itemSearched the item searched
-	 * @param numOfNearestNeighbor the number of 
 	 * @return double the prediction of the rating for the item searched for the specific user
 	 * @author Paolo Ronzoni
 	 */
-	public static int[][] predictionItemBasedValue(int[][] inputMatrix, int userColumnNumber, int userSearched, int itemcolumnNumber, int itemSearched) {		
+	public static double predictionItemBasedValue(int[][] inputMatrix, int userColumnNumber, int userSearched, int itemcolumnNumber, int itemSearched) {		
 	
 		// find the users matrtix of three columns userID, ItemID, rating
 		int[][] intermediateMatrix =  MatrixBuilder.userIDchoices(inputMatrix, userColumnNumber,userSearched);
 		
-		
-		
-//		// swap the userSearched position at the beginning of the usersVector
-//		for ( int i = 0; i < usersVector.length; i++) {
-//			if ( usersVector[i] == userSearched) {
-//				int tmp = usersVector[0];
-//				usersVector[0] = userSearched;
-//				usersVector[i] = tmp;
-//				break;
-//			}
-//		} // end for s
-//		
-//		// creates a matrix of three column: userSearched, allOtherUsers, userNearestNeighborValue
-//		double[][] intermediateMatrix = new double[usersVector.length -1][3];
-//		for (int row = 0; row < usersVector.length -1 ; row++) {
-//			intermediateMatrix[row][0] = userSearched;
-//			intermediateMatrix[row][1] = usersVector[row + 1];
-//			intermediateMatrix[row][2] = UserBasedNearestNeighbor.userNearestNeighborValue( inputMatrix, userColumnNumber,  userSearched, usersVector[row + 1]);
-//		}
-//		
-//		// orders the intermediateMatrix respect to userNearestNeighborValue
-//		int lastMatrixColumn = 5;
-//		double[][] finalMatrix = StatAndMathTools.sortMultidimensionArray(intermediateMatrix, 2);
-//		double[][] lastMatrix = new double[numOfNearestNeighbor][lastMatrixColumn];
-//		int lastRow = finalMatrix.length - 1;
-//		double[][] forMatrix;
-//		for ( int i = 0; i < numOfNearestNeighbor; i++) {	 
-//			forMatrix = MatrixBuilder.userIDchoicesDouble(inputMatrix, userColumnNumber,(int) finalMatrix[lastRow][1]);
-//			 while( Double.isNaN(finalMatrix[lastRow][2]) || 
-//					 !MatrixBuilder.isThereItem(forMatrix, 1, itemSearched)) {
-//				lastRow -= 1;
-//				forMatrix = MatrixBuilder.userIDchoicesDouble(inputMatrix, userColumnNumber,(int) finalMatrix[lastRow][1]);
-//			
-//			} // end while
-//			 
-//			// forMatrix is a  matrix of three column userID, itemID, rating
-//			 
-//
-//			 // || !MatrixBuilder.isThereItem(forMatrix, 1, 5)
-//		     // if () {
-//			 lastMatrix[i][0] = finalMatrix[lastRow][0]; // the user searched
-//			 lastMatrix[i][1] = finalMatrix[lastRow][1]; // an other users
-//			 lastMatrix[i][4] = finalMatrix[lastRow][2];
-//			 
-//			 lastMatrix[i][2] = MatrixBuilder.getItem(forMatrix, 1, itemSearched, 2);
-//			 lastMatrix[i][3] = StatAndMathTools.matrixColumnAverage(forMatrix,2 );
-//			 lastRow--;
-////		     } else {
-////				lastRow--;
-////			  }
-//		} // end for
-//		
-//		
-//		
 		int user1columnScores = 2;
 		int numRows = intermediateMatrix.length;
 		double numerator = 0;
 		double denominator = 0;
-
-		
-//		// calculates the averages of the columns' users scores 
-//		double user1Average = StatAndMathTools.matrixColumnAverage(intermediateMatrix, user1columnScores);
-//		double user2Average = StatAndMathTools.matrixColumnAverage(intermediateMatrix, user2columnScores);
 		
 		for (int row = 0; row < numRows  ; row++)
 		{
 	 
 		numerator += (adjustedCosineSimilarity( intermediateMatrix, userColumnNumber, itemSearched, intermediateMatrix[row][user1columnScores])  * intermediateMatrix[row][user1columnScores] );
 		
-		denominator += (intermediateMatrix[row][user1columnScores]);
+		denominator += adjustedCosineSimilarity( intermediateMatrix, userColumnNumber, itemSearched, intermediateMatrix[row][user1columnScores]);
 		
 			    	
 		} // end for loop
 		
 		
-		return intermediateMatrix;
+		return numerator / denominator;
 	 } // end method TMPpredictionUserBasedValue
 
 }// end class
