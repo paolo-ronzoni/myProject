1a35813dea5cbaea2433451f1404c4713acef798
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-09-10 14:43:15 +0200

    changes to method "meanAbsoluteErrorUserBased"

--- Recommender Systems/src/recommendationSystem/AccuracyMetrics.java
+++ Recommender Systems/src/recommendationSystem/AccuracyMetrics.java
@@ -7,6 +7,9 @@ public class AccuracyMetrics {
  * @param inputMatrix an input matrix of type int  with columns for users, products and ratings
  * @param usercolumnNumber the position of the column where to search the user
  * @param itemcolumnNumber the position of the column where to search the item
+ * @param numOfNearestNeighbor the number of similarity to use to calculate the predictionUserBasedValue
+ * @param userStart the first user where to start the computation of Mean Absolute Error 
+ * @param userEnd the last user used in the computation of Mean Absolute Error
  * @return double the weighted average of the differences between the actual scores and the predicted scores
  * @author Paolo Ronzoni
  */	
@@ -20,7 +23,7 @@ double denominator = 0;
 
 for ( int i = userStart; i <= userEnd; i++) {
 itemSearched = 1;
-UserBasedNearestNeighbor.predictionUserBasedValue(inputMatrix, userColumnNumber,  i, itemcolumnNumber, itemSearched, numOfNearestNeighbor);
+numerator += UserBasedNearestNeighbor.predictionUserBasedValue(inputMatrix, userColumnNumber,  i, itemcolumnNumber, itemSearched, numOfNearestNeighbor);
 
 }// end for
 
