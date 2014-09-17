eae7a4a38b5d0a2ea626701c61f4f8a80e6eae12
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-09-17 20:46:53 +0200

    minor changes

--- Recommender Systems/src/recommendationSystem/ItemBasedNearestNeighbor.java
+++ Recommender Systems/src/recommendationSystem/ItemBasedNearestNeighbor.java
@@ -120,6 +120,6 @@ public class ItemBasedNearestNeighbor {
 		
 		// 
 		return numerator / denominator;
-	 } //  end method TMPpredictionUserBasedValue
+	 } //  end method predictionItemBasedValue
 
 }// end class
