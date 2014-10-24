8c49d4836dbac6ab791095bd34352f7821374d5c
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-10-24 17:23:21 +0200

    minor changes

--- Recommender Systems/src/outputView/Test_03_UserBased.java
+++ Recommender Systems/src/outputView/Test_03_UserBased.java
@@ -12,8 +12,6 @@ public class Test_03_UserBased {
 	static String path1 = "DataStore\\u.txt";
 	static String splitExpression = "\\s+";
 		
-			
-			
 	public static void main(String[] args) {
 
 		String[][] myMatrix = IOMethods.txtMatrix(path1, splitExpression);
@@ -28,9 +26,9 @@ int firstUser = 301; // the first user to compute the similarity
 
 System.out.printf("This section test the correctness of the  result of the method\n\'userNearestNeighborValue\'  with the results of the book of Recommender Systems\n");
 double nearestValue;
-for (int secondUser = 2; secondUser <= 300; secondUser++) 
+for (int secondUser = 1; secondUser <= 301; secondUser++) 
 {
-	nearestValue = UserBasedNearestNeighbor.userNearestNeighborValue(secondMatrix, columnOfUsers, 1, secondUser);
+	nearestValue = UserBasedNearestNeighbor.userNearestNeighborValue(secondMatrix, columnOfUsers, firstUser, secondUser);
 		   System.out.printf("Similarity user %s and %s: %6.2f\n", firstUser, secondUser, nearestValue);
 } // end for
 
