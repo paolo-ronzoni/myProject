4d9049e77d0df4afd9c29be3f29d2c631cc8d797
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-10-24 21:14:49 +0200

    changes to class Test_12...

---
+++ Recommender Systems/src/outputView/Test_12_ItemBased.java
@@ -0,0 +1,44 @@
+package outputView;
+
+import recommendationSystem.*;
+import fileIO.IOMethods;
+
+import java.util.Arrays;
+
+/* This class test the formula pred(a,p) against the recommender systems database u.txt: 100,000 ratings (1-5) from 943 users on 1682 movies.
+ */
+
+public class Test_12_ItemBased {
+
+	static String path1 = "DataStore\\u.txt";
+	static String splitExpression = "\\s+";
+		
+			
+			
+	public static void main(String[] args) {
+
+		String[][] myMatrix = IOMethods.txtMatrix(path1, splitExpression);
+
+		
+//---input elements for the formula of userBasedSiamilarity------
+
+// the rectangular matrix of user, items and ratings
+int[][] secondMatrix = IOMethods.matrixConversion(myMatrix);
+int columnOfUsers = 0; // the column, in the rectangular input matrix, with the users number
+int firstUser = 1; // the first user to compute the similarity
+int secondUser = 2;
+int itemColumnNumber = 1;
+int itemSearched = 34;
+int numOfNearestNeighbor = 2;
+double predictedItemBasedValue;
+
+System.out.printf("This section test the formula pred(a,p) against the Tadatabase u.txt\nWhere the user change but  the item searched and the numOfNearestNeighbor\n are constant:\n");
+for( firstUser = 100; firstUser <= 200; firstUser++) 
+{
+	predictedItemBasedValue = ItemBasedNearestNeighbor.predictionItemBasedValue(secondMatrix, columnOfUsers, firstUser, itemColumnNumber, itemSearched, numOfNearestNeighbor);
+		System.out.printf("PredictionItemvalue: user=%s,item=%s,numOfRating=%s: %6.2f\n", firstUser, itemSearched, numOfNearestNeighbor, predictedItemBasedValue);
+}
+System.out.println(); // newline
+
+	} // end main
+} // end class Test_02_UserBased
