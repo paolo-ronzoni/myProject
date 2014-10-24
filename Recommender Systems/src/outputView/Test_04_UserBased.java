a14749e4759335688998fc164ca97ebf01a4a334
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-10-24 23:21:58 +0200

    format changes to class Test_04...

--- Recommender Systems/src/outputView/Test_04_UserBased.java
+++ Recommender Systems/src/outputView/Test_04_UserBased.java
@@ -32,9 +32,9 @@ int itemSearched = 5;
 int numOfNearestNeighbor = 2;
 double predictedUserBasedValue;
 
-System.out.printf("This section test the formula pred(a,p) against the Table 2.1.  (pag. 16\n from book \"Recommender Systems An Introduction\" by Jannach & al.)\nWhere \'a\' is the user, \'p\' is the item searched and numOfNearestNeighbor\nare the number of rating to take into account:\n");
+System.out.printf("This section test the formula pred(a,p) against the Table 2.1.  (pag. 16\nfrom book \"Recommender Systems An Introduction\" by Jannach & al.)\nWhere \'a\' is the user, \'p\' is the item searched and numOfNearestNeighbor\nare the number of rating to take into account:\n\n");
 predictedUserBasedValue = UserBasedNearestNeighbor.predictionUserBasedValue(secondMatrix, columnOfUsers, firstUser, itemColumnNumber, itemSearched, numOfNearestNeighbor);
-	System.out.printf("PredictionUserBasedvalue where the user is %s the item searched is %s\n and the neighbor's rating taken into accounts are %s: %6.2f\n", firstUser, itemSearched, numOfNearestNeighbor, predictedUserBasedValue);
+	System.out.printf("PredictionUserBasedValue (user=%3d,item=%4d,ratingUsed=%s): %6.2f\n", firstUser, itemSearched, numOfNearestNeighbor, predictedUserBasedValue);
 
 System.out.println("\n-----End of computation-----"); // newline
 
