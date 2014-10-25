271af795d306c1541552940cdf5aa6f943d995ae
Paolo Ronzoni <paolo.ronzoni18@gmail.com>
2014-10-25 14:09:03 +0200

    changes class Test 17 in ItemBased to UserBased

---
+++ Recommender Systems/src/outputView/Test_17_UserBased.java
@@ -0,0 +1,46 @@
+package outputView;
+
+import recommendationSystem.*;
+import fileIO.IOMethods;
+
+import java.util.Arrays;
+
+/* This class test the formula Mean Absolute Error (MAE) on the recommender systems database u1.txt: 20,000 ratings (1-5) from 943 users on 1682 movies.
+ */
+
+public class Test_17_UserBased {
+
+	static String path1 = "DataStore\\u1.txt";
+	static String splitExpression = "\\s+";
+		
+			
+			
+	public static void main(String[] args) {
+
+		String[][] myMatrix = IOMethods.txtMatrix(path1, splitExpression);
+
+		
+//---input elements for the formula of userBasedSiamilarity------
+
+// the rectangular matrix of user, items and ratings
+int[][] secondMatrix = IOMethods.matrixConversion(myMatrix);
+int columnOfUsers = 0; // the column, in the rectangular input matrix, with the users number
+int firstUser = 1; // the first user to compute the similarity
+int secondUser = 2;
+int itemColumnNumber = 1;
+int itemSearched = 34;
+int numOfNearestNeighbor = 2;
+double errorPrediction;
+
+System.out.printf("This section test the formula Mean Absolute Error (MAE) on the\ndatabase u1.txt when the user changes and the num of rating used\nare constant:\n\n");
+
+for( firstUser = 100; firstUser <= 105; firstUser++) 
+{
+	errorPrediction = AccuracyMetricsUserBased.meanAbsoluteErrorUserBasedOneUser(secondMatrix, columnOfUsers, firstUser, itemColumnNumber,numOfNearestNeighbor);
+		System.out.printf("MeanAbsoluteError (user=%3d,numOfRating=%s):%6.2f\n", firstUser, numOfNearestNeighbor, errorPrediction);
+}
+
+System.out.println("\n-----End of computation-----"); 
+
+	} // end main
+} // end class Test_17_UserBased
